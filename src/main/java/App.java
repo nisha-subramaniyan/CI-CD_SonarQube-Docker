b2b7a6fc0c86de9ef14ade767e8749030a672839
Nisha Subramaniyan <nishasubramaniyan.s@gmail.com>
2025-12-02 15:01:35 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
-public class App {
-    public static String greet() {
-        return "Hello, CI/CD with Jenkins, SonarQube, and Docker!";
-    }
-    
-    public static void main(String[] args) {
-        System.out.println(greet());
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testGreeting() {
+        String expected = "Hello from CI/CD Pipeline with Jenkins + SonarQube + Docker!";
+        assertEquals(expected, App.getGreeting());
     }
 }
