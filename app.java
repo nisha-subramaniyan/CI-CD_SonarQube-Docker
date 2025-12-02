a55a57c597c46c4e0c21f3977bc721c445a3d236
Nisha Subramaniyan <nishasubramaniyan.s@gmail.com>
2025-12-02 14:58:47 +0530

    Delete app.java

--- app.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static String greet() {
-        return "Hello, CI/CD with Jenkins, SonarQube, and Docker!";
-    }
-    
-    public static void main(String[] args) {
-        System.out.println(greet());
-    }
-}
