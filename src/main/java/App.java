0feb4343e56557e54efa1b8f91c401b31f652415
Nisha Subramaniyan <nishasubramaniyan.s@gmail.com>
2025-12-12 15:32:00 +0530

    Delete src directory

--- src/main/java/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static String getGreeting() {
-        return "Hello from CI/CD Pipeline with Jenkins + SonarQube + Docker!";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(App.getGreeting());
-    }
-}
